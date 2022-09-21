e01404802ad5e9350cf94a456ea14462dd9324ed
Dmitriy Petrov <nevermind1979@mail.ru>
2022-09-21 21:03:41 +0300

    refactor

--- src/main/java/Maps.java
+++ src/main/java/Maps.java
@@ -10,20 +10,20 @@ public class Maps {
     public void writeAndRead(Map<Integer,Integer>map){
         int [] arr = generate();
         new Thread(()->{
-            for (int i = 0; i <1000000 ; i++) {
+            for (int i = 0; i <1_000_000 ; i++) {
                 map.put(i,arr[i]);
             }
         }).start();
         new Thread(()-> {
-            for (int i = 0; i <1000000 ; i++) {
+            for (int i = 0; i <1_000_000 ; i++) {
                 map.get(i);
             }
         }).start();
     }
     public  int[] generate() {
-        int[] array = new int[1000000];
+        int[] array = new int[1_000_000];
         Random r = new Random();
-        for (int i = 0; i < 10000000; i++) {
+        for (int i = 0; i < 1_000_000; i++) {
             array[i] = r.nextInt(5000);
         }
         return array;
